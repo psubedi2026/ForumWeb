db6179d623fa52be5516b75fa1cb86986911f133
Prakash Subedi <prakash@theironyard.com>
2017-03-03 13:58:08 -0500

    ForumWeb Initial

---
+++ src/com/novauc/Message.java
@@ -0,0 +1,20 @@
+package com.novauc;
+
+/**
+ * Created by psubedi2020 on 3/2/17.
+ */
+public class Message {
+    int id;
+    int replyId;
+    String author;
+    String text;
+
+    public Message(int id, int replyId, String author, String text) {
+        this.id = id;
+        this.replyId = replyId;
+        this.author = author;
+        this.text = text;
+    }
+}
+
+
---
+++ src/com/novauc/User.java
@@ -0,0 +1,13 @@
+package com.novauc;
+
+/**
+ * Created by psubedi2020 on 3/2/17.
+ */
+public class User {
+    String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+}
+
